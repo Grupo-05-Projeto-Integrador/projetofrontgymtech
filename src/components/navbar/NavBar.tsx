c43b974d4f45263825b325de88ad8e2ca4369d2c
Yasminpontess <161251647+Yasminpontess@users.noreply.github.com>
2025-03-05 12:35:41 -0300

    Correção NavBar + Token

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,11 +1,11 @@
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
-import { useContext } from "react";
+import { ReactNode, useContext } from "react";
 
 function Navbar() {
   const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext);
+  const { aluno, handleLogout } = useContext(AuthContext);
 
   function logout() {
     handleLogout();
@@ -13,52 +13,60 @@ function Navbar() {
     navigate("/");
   }
 
-  return (
-    <>
-      <div className="w-full flex justify-center py-4 bg-[#090D28] text-white box-border max-h-18">
-        <div className="container flex justify-between items-center text-lg">
-          <img
-            src="src/assets/LOGO3.png"
-            className="max-h-24 w-auto pt-1"
-            alt="Logo"
-          />
+  let component: ReactNode;
 
-          <div className="flex gap-8">
-            <Link to="/home">
-              <p className="font-sans text-base hover:text-[#ea337b]">HOME</p>
-            </Link>
-            <Link to="/Sobre">
-              <p className="font-montserrat text-base hover:text-[#ea337b]">
-                SOBRE
-              </p>
-            </Link>
-            <Link to="/postagens">
-              <p className="font-montserrat text-base hover:text-[#ea337b]">
-                EXERCICIOS
-              </p>
-            </Link>
-            <Link to="/categoria">
-              <p className="font-montserrat text-base hover:text-[#ea337b]">
-                CATEGORIAS
-              </p>
-            </Link>
-            <Link to="/usuarios/all">
-              <p className="font-montserrat text-base hover:text-[#ea337b]">
-                PERFIL
-              </p>
-            </Link>
-            <Link
-              to=""
-              onClick={logout}
-              className="font-montserrat text-base hover:text-[#ea337b]"
-            >
-              SAIR
-            </Link>
+  if (aluno.token !== "") {
+    component = (
+      <>
+        <div className="w-full flex justify-center py-4 bg-[#090D28] text-white box-border max-h-18">
+          <div className="container flex justify-between items-center text-lg">
+            <img
+              src="src/assets/LOGO3.png"
+              className="max-h-24 w-auto pt-1"
+              alt="Logo"
+            />
+
+            <div className="flex gap-8">
+              <Link to="/home">
+                <p className="font-sans text-base hover:text-[#ea337b]">HOME</p>
+              </Link>
+              <Link to="/Sobre">
+                <p className="font-montserrat text-base hover:text-[#ea337b]">
+                  SOBRE
+                </p>
+              </Link>
+              <Link to="/postagens">
+                <p className="font-montserrat text-base hover:text-[#ea337b]">
+                  EXERCICIOS
+                </p>
+              </Link>
+              <Link to="/categoria">
+                <p className="font-montserrat text-base hover:text-[#ea337b]">
+                  CATEGORIAS
+                </p>
+              </Link>
+              <Link to="/usuarios/all">
+                <p className="font-montserrat text-base hover:text-[#ea337b]">
+                  PERFIL
+                </p>
+              </Link>
+              <Link
+                to=""
+                onClick={logout}
+                className="font-montserrat text-base hover:text-[#ea337b]"
+              >
+                SAIR
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    component = null;
+  }
+
+  return <>{component}</>;
 }
 
 export default Navbar;
